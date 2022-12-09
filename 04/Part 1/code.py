b1be8e077643ddffbcd0e4dc9587a52ef2608f67
ptrckmyr <pmayer2003@gmail.com>
2022-12-09 10:08:05 +0100

    Update code.py

--- 04/Part 1/code.py
+++ 04/Part 1/code.py
@@ -1,7 +1,16 @@
 with open("04\Part 1\input.txt") as file:
   pairs = [i for i in file.read().strip().split("\n")]
 
+print("Pars Liste VOR for schleife: ",pairs)
+
+for entry in pairs:
+  first, seccond = entry.split(",")
+  #print(first, seccond)
+  
+  first = [int(i) for i in first.split("-")]
+  seccond = [int(i) for i in seccond.split("-")]
+
+  print(first,seccond)
+  print("Split")
+  print(first)
 print(pairs)
-print(pairs[0])
-pair = [l.split(',') for l in ','.join(pairs).split(',')]
-print(pair)
