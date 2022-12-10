ad0692e3f779a85f969b3e11c1ebb7a61ebe8d24
ptrckmyr <pmayer2003@gmail.com>
2022-12-10 14:57:06 +0100

    Part 1 Fertig

--- 04/Part 1/code.py
+++ 04/Part 1/code.py
@@ -1,7 +1,10 @@
 with open("04\Part 1\input.txt") as file:
   pairs = [i for i in file.read().strip().split("\n")]
 
-print("Pars Liste VOR for schleife: ",pairs)
+print("Pairs Liste VOR for schleife: ",pairs)
+included = 0
+times = 0
+print(type(included))
 
 for entry in pairs:
   first, seccond = entry.split(",")
@@ -10,7 +13,21 @@ for entry in pairs:
   first = [int(i) for i in first.split("-")]
   seccond = [int(i) for i in seccond.split("-")]
 
-  print(first,seccond)
-  print("Split")
-  print(first)
-print(pairs)
+  if first[0] <= seccond[0] and first[1] >= seccond[1]:
+      included = included + 1
+      times += 1
+
+  elif seccond[0] <= first[0] and seccond[1] >= first[1]:
+      #print("Bin in der zweiten auch drin")
+      included = included + 1
+      times += 1
+
+  else:
+    pass
+      
+  print(first, seccond)
+  print("Der Counter sagt: ",included)
+
+print("So oft ist es durchgelafen: ", times)
+
+print("So oft ist es passiert: ", included)
