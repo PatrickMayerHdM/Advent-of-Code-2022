24db5bc7d9ff118b130e88a00d6ad71aa3ef3ea5
ptrckmyr <pmayer2003@gmail.com>
2022-12-19 22:18:48 +0100

    Update code.py

--- 04/Part 2/code.py
+++ 04/Part 2/code.py
@@ -1,30 +1,35 @@
 with open("04\Part 2\input.txt") as file:
   pairs = [i for i in file.read().strip().split("\n")]
 
-print("Pairs Liste VOR for schleife: ",pairs)
+#print("Pairs Liste VOR for schleife: ",pairs)
 included = 0
 times = 0
 
 for entry in pairs:
   first, seccond = entry.split(",")
   #print(first, seccond)
+  print(times)
   
-  main_first = [int(i) for i in first.split("-")]
-  main_seccond = [int(i) for i in seccond.split("-")]
+  first = [int(i) for i in first.split("-")]
+  seccond = [int(i) for i in seccond.split("-")]
 
-  for i in pairs:
+  if first[0] == seccond[1] or first[0] == seccond[0]:
+    times = times + 1
 
-    first = [int(i) for i in first.split("-")]
-    seccond = [int(i) for i in seccond.split("-")]
+  elif first[1] == seccond[0] or first[1] == seccond[1]:
+    times = times + 1
 
-    print("Der Main ist: ", main_first, "Der aktuelle ist: ", first)
+  elif first[0] <= seccond[0] and first[1] >= seccond[1]:
+    times = times + 1
 
+  elif first[0] <= seccond[0] and first[1] <= seccond[1]:
+    times = times + 1
 
-    
+  elif seccond[0] <= first[0] and seccond[1] >= first[1]:
+    times = times + 1
 
-      
-  print(first, seccond)
-  print("Der Counter sagt: ",included)
+  elif seccond[0] <= first[0] and seccond[1] <= first[1]:
+    times = times + 1
 
+print(times)
 
-print("So oft ist es passiert: ", included)
