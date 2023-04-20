f557f0f9d6961d70a08eb429392efdac459a7b18
ptrckmyr <pmayer2003@gmail.com>
2023-04-20 19:35:24 +0200

    Klasse Hockey entfernen

--- 16/Part 2/code.py
+++ 16/Part 2/code.py
@@ -1,18 +1,3 @@
 print("Tweatr")
 
 print("Twitter 2")
-
-class Hockey():
-    def get_t(self):
-        return self
-
-    def show_t(self):
-        print(self)
-
-    def ad(self):
-        sum = 1 + 1
-        print(sum)
-
-    def sub(self):
-        sum = 1 - 1
-        print(sum)
